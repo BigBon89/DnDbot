3fcaf55c5dd3c4c60381e820725cfb43a0907c6a
0x110 <Sashik95@yandex.ru>
2024-09-19 12:03:45 +0500

    errors fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        core cor = new core();
+        Core cor = new Core();
         System.out.printf(Integer.toString( cor.Random()));
     }
 }
