e4325642044fba826d10c9e6ac1d34a3f8cf75ec
0x110 <Sashik95@yandex.ru>
2024-09-19 12:02:41 +0500

    rename core to Core/ Random method for Core

---
+++ src/main/java/org/example/Core.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Core {
+    int Random() {
+        return 2;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.printf("Hello and welcome!");
+        core cor = new core();
+        System.out.printf(Integer.toString( cor.Random()));
     }
 }
