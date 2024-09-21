dc78e909a813453fa9cfc7735efdacd01c66632f
Axx <Sashik95@yandex.ru>
2024-09-21 22:02:43 +0500

    Added fields containing prefixes, bodies and suffixes of cities

--- src/main/java/org/example/CityNameGenerator.java
+++ src/main/java/org/example/CityNameGenerator.java
@@ -2,8 +2,24 @@ package org.example;
 
 public class CityNameGenerator
 {
-    CityNameGenerator()
+    public CityNameGenerator()
     {
 
     }
+
+    private static String[] prefixes =
+    {
+        "Dark", "Bright", "Storm", "Wind", "Shadow", "Sun", "Silver", "Iron", "Gold", "Wolf", "Dragon"
+    };
+
+    private static String[] roots =
+    {
+        "vale", "wood", "fall", "stone", "ford", "ridge", "keep", "haven", "burg", "crest", "cliff"
+    };
+
+    private static String[] suffixes =
+    {
+        "ton", "burgh", "port", "holm", "stead", "ford", "bridge", "field", "gate", "shire", "hill"
+    };
+
 }
