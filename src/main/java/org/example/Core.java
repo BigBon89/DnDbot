b809de1529183c55ff39ceba9641280f51a47d4e
Axx <Sashik95@yandex.ru>
2024-09-25 21:40:56 +0500

    included CharacterNameGenerator to Core, changed help command

--- src/main/java/org/example/Core.java
+++ src/main/java/org/example/Core.java
@@ -5,20 +5,24 @@ public class Core
     public Core()
     {
         cityNameGenerator = new CityNameGenerator();
-        Dice dice = new Dice();
+        dice = new Dice();
+        characterNameGenerator = new CharacterNameGenerator();
     }
     public void Input(String command)
     {
         if (command.equals("help"))
-            System.out.println("Commands:\nhelp\ngenerate city\nroll 'formula'");
+            System.out.println("Commands:\nhelp\ngenerate city\nroll 'formula'\ngenerate name");
         else if (command.equals("generate city"))
             System.out.println("Generated city name: " + cityNameGenerator.GenerateCityName());
         else if (command.split(" ")[0].equals("roll"))
             System.out.println("Rolled " );
+        else if (command.equals("generate name"))
+            System.out.println("Generated character name: " + characterNameGenerator.GenerateCharacterName());
         else
             System.out.println("Unknown command, type help for help");
     }
 
     private CityNameGenerator cityNameGenerator;
     private Dice dice;
+    private CharacterNameGenerator characterNameGenerator;
 }
