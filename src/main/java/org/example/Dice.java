3db6c829b8d6946eb6003d2ce9be82c053b517d5
0x110 <Sashik95@yandex.ru>
2024-09-26 12:04:31 +0500

    Dice refactor

--- src/main/java/org/example/Dice.java
+++ src/main/java/org/example/Dice.java
@@ -1,56 +1,46 @@
 package org.example;
+
 import java.util.Random;
 
-public class Dice
-{
-    public Integer RollDice(int diceCount, int dieType)
-    {
+public class Dice {
+    public Integer rollDice(int diceCount, int dieType) {
         Random random = new Random();
         int sum = 0;
-        for (int i = 0; i < diceCount; i++)
-        {
-            sum += random.nextInt(dieType)+1;
+        for (int i = 0; i < diceCount; i++) {
+            sum += random.nextInt(dieType) + 1;
         }
         return sum;
     }
-    public Integer Roll(String formula)
-    {
+
+    public Integer roll(String formula) {
         System.out.println("yes1");
         String[] dice = formula.split("\\+");
         int result = 0;
         System.out.println("yes2");
-        for(int i = 0; i < dice.length; i++)
-        {
-            if(dice[i].contains("d"))
-            {
+        for (int i = 0; i < dice.length; i++) {
+            if (dice[i].contains("d")) {
                 System.out.println("yes3");
-                result += RollDice(Integer.parseInt(dice[i].split("d")[0]), Integer.parseInt(dice[i].split("d")[1]));
+                result += rollDice(Integer.parseInt(dice[i].split("d")[0]), Integer.parseInt(dice[i].split("d")[1]));
                 System.out.println("yes4");
-            }
-            else
-            {
+            } else {
                 result += Integer.parseInt(dice[i]);
             }
         }
         return result;
     }
-    public Integer D20Test(int state, int modifier) //state: 0 - normal, 1 - advantage, -1 - disadvantage
+
+    public Integer d20Test(int state, int modifier) //state: 0 - normal, 1 - advantage, -1 - disadvantage
     {
         Random random = new Random();
         int result = 0;
-        if(state == 1)
-        {
-            result = random.nextInt(20)+1;
-            result = Math.max(result, random.nextInt(20)+1);
-        }
-        else if(state == -1)
-        {
-            result = random.nextInt(20)+1;
-            result = Math.min(result, random.nextInt(20)+1);
-        }
-        else
-        {
-            result = random.nextInt(20)+1;
+        if (state == 1) {
+            result = random.nextInt(20) + 1;
+            result = Math.max(result, random.nextInt(20) + 1);
+        } else if (state == -1) {
+            result = random.nextInt(20) + 1;
+            result = Math.min(result, random.nextInt(20) + 1);
+        } else {
+            result = random.nextInt(20) + 1;
         }
         return result + modifier;
     }
