12bad47227825526396c9aa7e4edff7b3300bfdf
0x110 <Sashik95@yandex.ru>
2024-09-26 13:43:58 +0500

    class CityNameGenerator: changed random to field, added constructor

--- src/main/java/org/example/CityNameGenerator.java
+++ src/main/java/org/example/CityNameGenerator.java
@@ -1,31 +1,34 @@
 package org.example;
+
 import java.util.Random;
 
-public class CityNameGenerator
-{
-    //TODO: вынести Random в поле
-    public String generateCityName()
+public class CityNameGenerator {
+    private Random random;
+
+    private final String[] prefixes =
+            {
+                    "Dark", "Bright", "Storm", "Wind", "Shadow", "Sun", "Silver", "Iron", "Gold", "Wolf", "Dragon"
+            };
+
+    private final String[] roots =
+            {
+                    "vale", "wood", "fall", "stone", "ford", "ridge", "keep", "haven", "burg", "crest", "cliff"
+            };
+
+    private final String[] suffixes =
+            {
+                    "ton", "burgh", "port", "holm", "stead", "ford", "bridge", "field", "gate", "shire", "hill"
+            };
+
+    public CityNameGenerator()
     {
-        Random random = new Random();
+        random = new Random();
+    }
+
+    public String generateCityName() {
         String prefix = prefixes[random.nextInt(prefixes.length)];
         String root = roots[random.nextInt(roots.length)];
         String suffix = suffixes[random.nextInt(suffixes.length)];
         return prefix + root + suffix;
     }
-
-    private static String[] prefixes =
-    {
-        "Dark", "Bright", "Storm", "Wind", "Shadow", "Sun", "Silver", "Iron", "Gold", "Wolf", "Dragon"
-    };
-
-    private static String[] roots =
-    {
-        "vale", "wood", "fall", "stone", "ford", "ridge", "keep", "haven", "burg", "crest", "cliff"
-    };
-
-    private static String[] suffixes =
-    {
-        "ton", "burgh", "port", "holm", "stead", "ford", "bridge", "field", "gate", "shire", "hill"
-    };
-
 }
