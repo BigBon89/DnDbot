b22e9a3bcfbc83ea0c9ed46a27734b9367085b0a
0x110 <Sashik95@yandex.ru>
2024-09-26 14:01:32 +0500

    added constructor and field for TestCharacterNameGenerator

--- src/test/java/org/example/TestCharacterNameGenerator.java
+++ src/test/java/org/example/TestCharacterNameGenerator.java
@@ -5,8 +5,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TestCharacterNameGenerator {
+    private CharacterNameGenerator characterNameGenerator;
+
+    TestCharacterNameGenerator() {
+        characterNameGenerator = new CharacterNameGenerator();
+    }
+
     public void check() {
-        CharacterNameGenerator characterNameGenerator = new CharacterNameGenerator();
         String res = characterNameGenerator.generateCharacterName();
         assertNotNull(res);
     }
