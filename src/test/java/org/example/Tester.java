40ea82217c3f460cc5b00f2627be3f75e05587b5
0x110 <Sashik95@yandex.ru>
2024-09-26 12:04:18 +0500

    Added tests

--- src/test/java/org/example/Tester.java
+++ src/test/java/org/example/Tester.java
@@ -1,13 +1,25 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class Tester {
-    CharacterNameGenerator characterNameGenerator;
 @Test
-    public void check() {
-
+    public void checkCharacterNameGenerator() {
+        CharacterNameGenerator characterNameGenerator = new CharacterNameGenerator();
+        String res = characterNameGenerator.generateCharacterName();
+        assertNotNull(res);
     }
-
-    private Core core;
+@Test
+    public void checkCityNameGenerator() {
+        CityNameGenerator cityNameGenerator = new CityNameGenerator();
+        String res = cityNameGenerator.generateCityName();
+        assertNotNull(res);
+    }
+@Test
+    public void checkRoll() {
+        Dice dice = new Dice();
+        int res = dice.roll("2d4");
+        assertNotNull(res);
+    }
 }
