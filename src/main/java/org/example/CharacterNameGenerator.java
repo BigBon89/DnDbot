b634824b19e9f2a4a3195b639f23fd50430bed7e
0x110 <Sashik95@yandex.ru>
2024-09-26 13:40:01 +0500

    changed field positions for class CharacterNameGenerator

--- src/main/java/org/example/CharacterNameGenerator.java
+++ src/main/java/org/example/CharacterNameGenerator.java
@@ -3,14 +3,6 @@ package org.example;
 import java.util.Random;
 
 public class CharacterNameGenerator {
-    //TODO: вынести Random в поле
-    public String generateCharacterName() {
-        Random random = new Random();
-        String firstName = firstNames[random.nextInt(firstNames.length)];
-        String lastName = lastNames[random.nextInt(lastNames.length)];
-        return firstName + " " + lastName;
-    }
-
     private final String[] firstNames =
             {
                     "Aragorn", "Elwyn", "Frodo", "Gandalf", "Legolas", "Boromir", "Thranduil", "Galadriel", "Eowyn", "Gimli"
@@ -20,4 +12,12 @@ public class CharacterNameGenerator {
             {
                     "Stormwind", "Oakenshield", "Elfsong", "Silverblade", "Brightstar", "Ironfoot", "Dragonslayer", "Shadowalker"
             };
+
+    //TODO: вынести Random в поле
+    public String generateCharacterName() {
+        Random random = new Random();
+        String firstName = firstNames[random.nextInt(firstNames.length)];
+        String lastName = lastNames[random.nextInt(lastNames.length)];
+        return firstName + " " + lastName;
+    }
 }
