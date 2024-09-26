2d2a76a792a9244e6d54c9d78587684b4d69fc81
0x110 <Sashik95@yandex.ru>
2024-09-26 10:54:10 +0500

    fix character name generator

--- src/main/java/org/example/Core.java
+++ src/main/java/org/example/Core.java
@@ -6,19 +6,23 @@ public class Core
     {
         cityNameGenerator = new CityNameGenerator();
         dice = new Dice();
+        characterNameGenerator = new CharacterNameGenerator();
     }
     public void Input(String command)
     {
         if (command.equals("help"))
-            System.out.println("Commands:\nhelp\ngenerate city\nroll 'formula'");
+            System.out.println("Commands:\nhelp\ngenerate city\nroll 'formula'\ngenerate name");
         else if (command.equals("generate city"))
             System.out.println("Generated city name: " + cityNameGenerator.GenerateCityName());
         else if (command.split(" ")[0].equals("roll"))
             System.out.println("Rolled " + dice.Roll(command.split(" ")[1]) );
+        else if (command.equals("generate name"))
+            System.out.println("Generated character name: " + characterNameGenerator.GenerateCharacterName());
         else
             System.out.println("Unknown command, type help for help");
     }
 
     private CityNameGenerator cityNameGenerator;
+    private CharacterNameGenerator characterNameGenerator;
     private Dice dice;
 }
