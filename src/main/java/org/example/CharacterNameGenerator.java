3cdf30fce4e8512d04ab1878399b32bc3dda8e3a
0x110 <Sashik95@yandex.ru>
2024-09-26 13:41:58 +0500

    changed random to field, added constructor

--- src/main/java/org/example/CharacterNameGenerator.java
+++ src/main/java/org/example/CharacterNameGenerator.java
@@ -3,6 +3,8 @@ package org.example;
 import java.util.Random;
 
 public class CharacterNameGenerator {
+    private Random random;
+
     private final String[] firstNames =
             {
                     "Aragorn", "Elwyn", "Frodo", "Gandalf", "Legolas", "Boromir", "Thranduil", "Galadriel", "Eowyn", "Gimli"
@@ -13,9 +15,13 @@ public class CharacterNameGenerator {
                     "Stormwind", "Oakenshield", "Elfsong", "Silverblade", "Brightstar", "Ironfoot", "Dragonslayer", "Shadowalker"
             };
 
+    public CharacterNameGenerator()
+    {
+        random = new Random();
+    }
+
     //TODO: вынести Random в поле
     public String generateCharacterName() {
-        Random random = new Random();
         String firstName = firstNames[random.nextInt(firstNames.length)];
         String lastName = lastNames[random.nextInt(lastNames.length)];
         return firstName + " " + lastName;
