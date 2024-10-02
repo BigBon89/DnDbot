b91e762670e99e9b80bc1f43c21219decc050844
Axx <Sashik95@yandex.ru>
2024-10-02 23:31:59 +0500

    delete in object in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         BotLogicalCore botLogicalCore = new BotLogicalCore();
 
         botLogicalCore.start();
