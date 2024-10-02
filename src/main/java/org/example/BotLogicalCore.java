39cd8b9443e6d3980f73d9b0802ae29930da9d2b
Axx <Sashik95@yandex.ru>
2024-10-02 23:31:02 +0500

    rename console to ioHandler

--- src/main/java/org/example/BotLogicalCore.java
+++ src/main/java/org/example/BotLogicalCore.java
@@ -4,33 +4,33 @@ public class BotLogicalCore {
     private final CityNameGenerator cityNameGenerator;
     private final CharacterNameGenerator characterNameGenerator;
     private final Dice dice;
-    private final Console console;
+    private final Console ioHandler;
 
     public BotLogicalCore() {
         cityNameGenerator = new CityNameGenerator();
         characterNameGenerator = new CharacterNameGenerator();
         dice = new Dice();
-        console = new Console();
+        ioHandler = new Console();
     }
 
     public void start() {
         while (true) {
-            commandHandler(console.getText());
+            commandHandler(ioHandler.getText());
         }
     }
 
     public void commandHandler(String command) {
         if (command.equals("help"))
-            System.out.println("Commands:\nhelp\ngenerate city\nroll 'formula'\nd20 'modifier' 'normal/advantage/disadvantage'\ngenerate name");
+            ioHandler.print("Commands:\nhelp\ngenerate city\nroll 'formula'\nd20 'modifier' 'normal/advantage/disadvantage'\ngenerate name");
         else if (command.equals("generate city"))
-            System.out.println("Generated city name: " + cityNameGenerator.generateName());
+            ioHandler.print("Generated city name: " + cityNameGenerator.generateName());
         else if (command.split(" ")[0].equals("roll"))
-            System.out.println("Rolled " + dice.roll(new DiceCombination(command.split(" ")[1])));
+            ioHandler.print("Rolled " + dice.roll(new DiceCombination(command.split(" ")[1])));
         else if (command.split(" ")[0].equals("d20"))
-            System.out.println("Rolled " + dice.d20Test(Integer.valueOf(command.split(" ")[1]), Dice.D20State.valueOf(command.split(" ")[2])));
+            ioHandler.print("Rolled " + dice.d20Test(Integer.valueOf(command.split(" ")[1]), Dice.D20State.valueOf(command.split(" ")[2])));
         else if (command.equals("generate name"))
-            System.out.println("Generated character name: " + characterNameGenerator.generateName());
+            ioHandler.print("Generated character name: " + characterNameGenerator.generateName());
         else
-            System.out.println("Unknown command, type help for help");
+            ioHandler.print("Unknown command, type help for help");
     }
 }
