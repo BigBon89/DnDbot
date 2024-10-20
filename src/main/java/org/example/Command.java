d102bed05775b924af0e2ff679d47a46da73200c
Axx <Sashik95@yandex.ru>
2024-10-20 13:33:30 +0500

    Command constructor optimization: replacing loop with System.arraycopy

--- src/main/java/org/example/Command.java
+++ src/main/java/org/example/Command.java
@@ -8,11 +8,9 @@ public class Command {
     Command(String inputLine) {
         String[] words = inputLine.split(" ");
         command = words[0];
-        arguments = new String[words.length - 1];
         argumentsCount = words.length - 1;
-        for (int i = 0; i < argumentsCount; i++){
-            arguments[i] = words[i+1];
-        }
+        arguments = new String[argumentsCount];
+        System.arraycopy(words, 1, arguments, 0, argumentsCount);
     }
 
     String getCommand() {
