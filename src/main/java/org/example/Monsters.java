24e325fd567c9a3543688620792f55d65319f017
0x110 <Sashik95@yandex.ru>
2024-11-07 11:57:23 +0500

    added Monsters class

---
+++ src/main/java/org/example/Monsters.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Monsters {
+    Monster[] monsters;
+
+    public void Monsters(EncounterDifficulty difficulty, Integer playersCount, Integer playersLevel, String monsterFilter) {
+        monsters = new Monster[1]; //TODO: случайно заполнить
+
+    }
+}
