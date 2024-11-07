02fe4d6644d1d1130fe1d48e6c3a8ed4bb89cb66
0x110 <Sashik95@yandex.ru>
2024-11-07 10:53:37 +0500

    Monster class

---
+++ src/main/java/org/example/Monster.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Monster {
+    private String type;
+    private String name;
+    private Integer challangeRating;
+    private Integer health;
+    private Integer maxHealth;
+}
