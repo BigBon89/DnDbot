cd3245cec3767b3f710e5a770f41f5e3059b68ff
Madness42 <purplemadness42@gmail.com>
2024-11-10 17:25:46 +0500

    CR Generation

--- src/main/java/org/example/Monsters.java
+++ src/main/java/org/example/Monsters.java
@@ -1,13 +1,36 @@
 package org.example;
 
+import java.util.Random;
+
 public class Monsters {
     Monster[] monsters;
-    double[] easyCRs = {0.125, 0.125, 0.25, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5};
-    double[] normalCRs = {0.125, 0.25, 0.5, 0.75, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9};
-    double[] hardCRs = {0.25, 0.5, 0.75, 1, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10};
-    double[] CRCaps = {1, 3, 4, 6, 8, 9, 10, 12, 13, 15, 16, 17, 19, 20, 22, 24, 25, 26, 28, 30};
-    public void Monsters(EncounterDifficulty difficulty, Integer playersCount, Integer playersLevel, String monsterFilter) {
+
+    protected Random random;
+    public Monsters() {random = new Random();}
+    public Monsters(int seed) {random = new Random(seed);}
+
+    int[] easyCRs = {1, 1, 2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68};
+    int[] normalCRs = {1, 2, 4, 6, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72};
+    int[] hardCRs = {2, 4, 6, 8, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80};
+    int[] CRCaps = {8, 24, 32, 48, 64, 72, 80, 96, 104, 120, 128, 136, 152, 160, 176, 192, 200, 208, 224, 240};
+
+    public void Generate(EncounterDifficulty difficulty, Integer playersCount, Integer playersLevel, String monsterFilter) {
         monsters = new Monster[1]; //TODO: случайно заполнить
+        int CRBudget = 0;
+        int CRCap = CRCaps[playersLevel]*8;
+        switch (difficulty){
+            case EASY -> CRBudget = easyCRs[playersLevel] * playersCount;
+            case NORMAL -> CRBudget = normalCRs[playersLevel] * playersCount;
+            case HARD -> CRBudget = hardCRs[playersLevel] * playersCount;
+        }
+
+        int numberOfUniqueMonsters = random.nextInt(4) + 1  + (CRBudget/CRCap);
+        int[] uniqueMonstersCR = new int[numberOfUniqueMonsters];
+        int minCR = (CRBudget - CRCap) / (numberOfUniqueMonsters - 1);
+        int maxCR = CRBudget / numberOfUniqueMonsters;
+        for (int i = 0; i < numberOfUniqueMonsters; i++) {
+            uniqueMonstersCR[i] = random.nextInt(maxCR - minCR + 1) + minCR;
+        }
 
     }
 
