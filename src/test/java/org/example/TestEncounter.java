3166f8ce62b37c10fbaf7b5537b6dc8ae32e1770
Madness42 <purplemadness42@gmail.com>
2024-11-25 21:46:45 +0500

    убрал принты в тестах

--- src/test/java/org/example/TestEncounter.java
+++ src/test/java/org/example/TestEncounter.java
@@ -16,7 +16,6 @@ public class TestEncounter {
     @Test
     public void checkEncounter() {
         String res = encounter.start(EncounterDifficulty.valueOf("NORMAL"), 4, 1, "");
-        System.out.println(res);
         assertEquals("0. Kobold Veles 1/1\n" +
                 "1. Stirge 1/1\n" +
                 "2. Blood-Borne Ooze 2/2", res);
@@ -26,7 +25,6 @@ public class TestEncounter {
     public void checkAttack() {
         encounter.start(EncounterDifficulty.valueOf("NORMAL"), 4, 1, "");
         String res = encounter.attack(2, 4);
-        System.out.println(res);
         assertEquals("0. Kobold Veles 1/1\n" +
                 "1. Stirge 1/1\n" +
                 "2. Blood-Borne Ooze (DEAD)", res);
@@ -36,7 +34,6 @@ public class TestEncounter {
     public void checkWrongIndex() {
         encounter.start(EncounterDifficulty.valueOf("NORMAL"), 4, 1, "");
         String res = encounter.attack(5, 9);
-        System.out.println(res);
         assertEquals("Wrong monster index", res);
     }
 }
