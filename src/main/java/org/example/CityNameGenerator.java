504bce1cc043fc896a6f28e384fb328646efbc84
Axx <Sashik95@yandex.ru>
2024-11-26 21:04:30 +0500

    code style update

--- src/main/java/org/example/CityNameGenerator.java
+++ src/main/java/org/example/CityNameGenerator.java
@@ -15,16 +15,16 @@ public class CityNameGenerator extends NameGenerator {
 
     public void initializeNames() {
         prefixes = new String[]{
-        "Dark", "Bright", "Storm", "Wind", "Shadow", "Sun",
-        "Silver", "Iron", "Gold", "Wolf", "Dragon"
+            "Dark", "Bright", "Storm", "Wind", "Shadow", "Sun",
+            "Silver", "Iron", "Gold", "Wolf", "Dragon"
         };
         roots = new String[]{
-        "vale", "wood", "fall", "stone", "ford", "ridge",
-        "keep", "haven", "burg", "crest", "cliff"
+            "vale", "wood", "fall", "stone", "ford", "ridge",
+            "keep", "haven", "burg", "crest", "cliff"
         };
         suffixes = new String[]{
-                "ton", "burgh", "port", "holm", "stead", "ford",
-                "bridge", "field", "gate", "shire", "hill"
+            "ton", "burgh", "port", "holm", "stead", "ford",
+            "bridge", "field", "gate", "shire", "hill"
         };
     }
 }
