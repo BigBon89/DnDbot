8f7b38b48951b49a07e52db08883cb7c3eb9bf95
Axx <Sashik95@yandex.ru>
2024-11-26 21:06:31 +0500

    code style update

--- src/main/java/org/example/CharacterNameGenerator.java
+++ src/main/java/org/example/CharacterNameGenerator.java
@@ -12,14 +12,13 @@ public class CharacterNameGenerator extends NameGenerator {
     }
 
     public void initializeNames() {
-        prefixes = new String[]{"Aragorn", "Elwyn", "Frodo",
-                "Gandalf", "Legolas", "Boromir",
-                "Thranduil", "Galadriel", "Eowyn",
-                "Gimli"
+        prefixes = new String[]{
+            "Aragorn", "Elwyn", "Frodo", "Gandalf", "Legolas", "Boromir",
+            "Thranduil", "Galadriel", "Eowyn", "Gimli"
         };
-        roots = new String[]{" Stormwind", " Oakenshield", " Elfsong",
-                " Silverblade", " Brightstar", " Ironfoot",
-                " Dragonslayer", " Shadowalker"
+        roots = new String[] {
+            " Stormwind", " Oakenshield", " Elfsong", " Silverblade", " Brightstar",
+            " Ironfoot", " Dragonslayer", " Shadowalker"
         };
         suffixes = new String[]{""};
     }
