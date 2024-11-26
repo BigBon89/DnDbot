013b23e041163afa4851ecb69458d61cb2767428
Axx <Sashik95@yandex.ru>
2024-11-26 22:00:32 +0500

    code style update

--- src/main/java/org/example/D20State.java
+++ src/main/java/org/example/D20State.java
@@ -1,3 +1,5 @@
 package org.example;
 
-public enum D20State {NORMAL, ADVANTAGE, DISADVANTAGE}
+public enum D20State {
+    NORMAL, ADVANTAGE, DISADVANTAGE
+}
--- src/main/java/org/example/InputOutput.java
+++ src/main/java/org/example/InputOutput.java
@@ -2,5 +2,6 @@ package org.example;
 
 public interface InputOutput {
     public void print(String text);
+
     public String getText();
 }
