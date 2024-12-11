82d470d0443c495483162d55189e8548be4f0553
Axx <Sashik95@yandex.ru>
2024-12-11 21:41:25 +0500

    moved commandHandler to a separate class

--- src/main/java/org/example/BotLogicalCore.java
+++ src/main/java/org/example/BotLogicalCore.java
@@ -1,12 +1,8 @@
 package org.example;
 
 public class BotLogicalCore {
-    private final CityNameGenerator cityNameGenerator;
-    private final CharacterNameGenerator characterNameGenerator;
-    private final ClassNameGenerator classNameGenerator;
-    private final Dice dice;
+    private final CommandHandler commandHandler;
     private final InputOutput ioHandler;
-    private Encounter encounter;
 
     public BotLogicalCore(CityNameGenerator cityNameGenerator,
         CharacterNameGenerator characterNameGenerator,
@@ -15,64 +11,28 @@ public class BotLogicalCore {
         InputOutput inputOutput,
         Encounter encounter
     ) {
-        this.cityNameGenerator = cityNameGenerator;
-        this.characterNameGenerator = characterNameGenerator;
-        this.classNameGenerator = classNameGenerator;
-        this.dice = dice;
         this.ioHandler = inputOutput;
-        this.encounter = encounter;
+        this.commandHandler = new CommandHandler(
+                cityNameGenerator,
+                characterNameGenerator,
+                classNameGenerator,
+                dice, encounter
+        );
     }
 
-    public void start() {
+    public void startConsole() {
         while (true) {
             Command command = new Command(ioHandler.getText());
-            String resultOutput = commandHandler(command);
+            String resultOutput = commandHandler.handleCommand(command);
             ioHandler.print(resultOutput);
         }
     }
 
-    public String commandHandler(Command command) {
-        if (!command.isValid()) {
-            return "Unknown command, type 'help' for available commands.";
-        }
-        try {
-            switch (command.getCommandType()) {
-                case HELP:
-                    return Commands.helpCommand();
-                case GENERATE_CITY:
-                    return "Generated city name: " + cityNameGenerator.generateName();
-                case GENERATE_CLASS:
-                    return "Generated class name: " + classNameGenerator.generateName();
-                case ROLL:
-                    return dice.roll(new DiceCombination(command.getArguments()[0]));
-                case D20:
-                    return "Rolled " + dice.d20Test(Integer.parseInt(command.getArguments()[0]),
-                            D20State.valueOf(command.getArguments()[1])
-                    );
-                case GENERATE_NAME:
-                    return "Generated character name: " + characterNameGenerator.generateName();
-                case GENERATE_ENCOUNTER:
-                    return encounter.start(EncounterDifficulty.valueOf(command.getArguments()[0]),
-                            Integer.parseInt(command.getArguments()[1]), Integer.parseInt(command.getArguments()[2]),
-                            ""
-                    );
-                case GENERATE_ENCOUNTER_FILTER:
-                    return encounter.start(EncounterDifficulty.valueOf(command.getArguments()[0]),
-                            Integer.parseInt(command.getArguments()[1]),
-                            Integer.parseInt(command.getArguments()[2]),
-                            command.getArguments()[3]
-                    );
-                case ENCOUNTER_END:
-                    return encounter.end();
-                case ATTACK:
-                    return encounter.attack(Integer.parseInt(command.getArguments()[0]),
-                            Integer.parseInt(command.getArguments()[1]
-                    ));
-                default:
-                    return "Unknown command, type help for help";
-            }
-        } catch (IllegalArgumentException e) {
-            return "Unknown command, type help for help";
+    public void startGui() {
+        while (true) {
+            Command command = new Command(ioHandler.getText());
+            String resultOutput = commandHandler.handleCommand(command);
+            ioHandler.print(resultOutput);
         }
     }
 }
---
+++ src/main/java/org/example/CommandHandler.java
@@ -0,0 +1,67 @@
+package org.example;
+
+public class CommandHandler {
+    private final CityNameGenerator cityNameGenerator;
+    private final CharacterNameGenerator characterNameGenerator;
+    private final ClassNameGenerator classNameGenerator;
+    private final Dice dice;
+    private final Encounter encounter;
+
+    public CommandHandler(CityNameGenerator cityNameGenerator,
+        CharacterNameGenerator characterNameGenerator,
+        ClassNameGenerator classNameGenerator,
+        Dice dice,
+        Encounter encounter
+    ) {
+        this.cityNameGenerator = cityNameGenerator;
+        this.characterNameGenerator = characterNameGenerator;
+        this.classNameGenerator = classNameGenerator;
+        this.dice = dice;
+        this.encounter = encounter;
+    }
+
+    public String handleCommand(Command command) {
+        if (!command.isValid()) {
+            return "Unknown command, type 'help' for available commands.";
+        }
+        try {
+            switch (command.getCommandType()) {
+                case HELP:
+                    return Commands.helpCommand();
+                case GENERATE_CITY:
+                    return "Generated city name: " + cityNameGenerator.generateName();
+                case GENERATE_CLASS:
+                    return "Generated class name: " + classNameGenerator.generateName();
+                case ROLL:
+                    return dice.roll(new DiceCombination(command.getArguments()[0]));
+                case D20:
+                    return "Rolled " + dice.d20Test(Integer.parseInt(command.getArguments()[0]),
+                            D20State.valueOf(command.getArguments()[1])
+                    );
+                case GENERATE_NAME:
+                    return "Generated character name: " + characterNameGenerator.generateName();
+                case GENERATE_ENCOUNTER:
+                    return encounter.start(EncounterDifficulty.valueOf(command.getArguments()[0]),
+                            Integer.parseInt(command.getArguments()[1]), Integer.parseInt(command.getArguments()[2]),
+                            ""
+                    );
+                case GENERATE_ENCOUNTER_FILTER:
+                    return encounter.start(EncounterDifficulty.valueOf(command.getArguments()[0]),
+                            Integer.parseInt(command.getArguments()[1]),
+                            Integer.parseInt(command.getArguments()[2]),
+                            command.getArguments()[3]
+                    );
+                case ENCOUNTER_END:
+                    return encounter.end();
+                case ATTACK:
+                    return encounter.attack(Integer.parseInt(command.getArguments()[0]),
+                            Integer.parseInt(command.getArguments()[1]
+                            ));
+                default:
+                    return "Unknown command, type help for help";
+            }
+        } catch (IllegalArgumentException e) {
+            return "Unknown command, type help for help";
+        }
+    }
+}
