2cf65870c6dcfd55a98a28441371f9ba7161acf1
Axx <Sashik95@yandex.ru>
2024-12-12 23:14:05 +0500

    code style update

--- src/main/java/org/example/CommandHandler.java
+++ src/main/java/org/example/CommandHandler.java
@@ -8,10 +8,10 @@ public class CommandHandler {
     private final Encounter encounter;
 
     public CommandHandler(CityNameGenerator cityNameGenerator,
-        CharacterNameGenerator characterNameGenerator,
-        ClassNameGenerator classNameGenerator,
-        Dice dice,
-        Encounter encounter
+                          CharacterNameGenerator characterNameGenerator,
+                          ClassNameGenerator classNameGenerator,
+                          Dice dice,
+                          Encounter encounter
     ) {
         this.cityNameGenerator = cityNameGenerator;
         this.characterNameGenerator = characterNameGenerator;
@@ -25,41 +25,31 @@ public class CommandHandler {
             return "Unknown command, type 'help' for available commands.";
         }
         try {
-            switch (command.getCommandType()) {
-                case HELP:
-                    return Commands.helpCommand();
-                case GENERATE_CITY:
-                    return "Generated city name: " + cityNameGenerator.generateName();
-                case GENERATE_CLASS:
-                    return "Generated class name: " + classNameGenerator.generateName();
-                case ROLL:
-                    return dice.roll(new DiceCombination(command.getArguments()[0]));
-                case D20:
-                    return "Rolled " + dice.d20Test(Integer.parseInt(command.getArguments()[0]),
-                            D20State.valueOf(command.getArguments()[1])
+            return switch (command.getCommandType()) {
+                case HELP -> Commands.helpCommand();
+                case GENERATE_CITY -> "Generated city name: " + cityNameGenerator.generateName();
+                case GENERATE_CLASS -> "Generated class name: " + classNameGenerator.generateName();
+                case ROLL -> dice.roll(new DiceCombination(command.getArguments()[0]));
+                case D20 -> "Rolled " + dice.d20Test(Integer.parseInt(command.getArguments()[0]),
+                    D20State.valueOf(command.getArguments()[1])
+                );
+                case GENERATE_NAME -> "Generated name: " + characterNameGenerator.generateName();
+                case GENERATE_ENCOUNTER -> encounter.start(EncounterDifficulty.valueOf(command.getArguments()[0]),
+                    Integer.parseInt(command.getArguments()[1]), Integer.parseInt(command.getArguments()[2]),
+                    ""
+                );
+                case GENERATE_ENCOUNTER_FILTER ->
+                    encounter.start(EncounterDifficulty.valueOf(command.getArguments()[0]),
+                        Integer.parseInt(command.getArguments()[1]),
+                        Integer.parseInt(command.getArguments()[2]),
+                        command.getArguments()[3]
                     );
-                case GENERATE_NAME:
-                    return "Generated name: " + characterNameGenerator.generateName();
-                case GENERATE_ENCOUNTER:
-                    return encounter.start(EncounterDifficulty.valueOf(command.getArguments()[0]),
-                            Integer.parseInt(command.getArguments()[1]), Integer.parseInt(command.getArguments()[2]),
-                            ""
-                    );
-                case GENERATE_ENCOUNTER_FILTER:
-                    return encounter.start(EncounterDifficulty.valueOf(command.getArguments()[0]),
-                            Integer.parseInt(command.getArguments()[1]),
-                            Integer.parseInt(command.getArguments()[2]),
-                            command.getArguments()[3]
-                    );
-                case ENCOUNTER_END:
-                    return encounter.end();
-                case ATTACK:
-                    return encounter.attack(Integer.parseInt(command.getArguments()[0]),
-                            Integer.parseInt(command.getArguments()[1]
-                            ));
-                default:
-                    return "Unknown command, type help for help";
-            }
+                case ENCOUNTER_END -> encounter.end();
+                case ATTACK -> encounter.attack(Integer.parseInt(command.getArguments()[0]),
+                    Integer.parseInt(command.getArguments()[1]
+                ));
+                default -> "Unknown command, type help for help";
+            };
         } catch (IllegalArgumentException e) {
             return "Unknown command, type help for help";
         }
