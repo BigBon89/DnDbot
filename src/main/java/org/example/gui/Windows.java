65ec9d591472a8658c01b8f876ef660d066d4203
Axx <Sashik95@yandex.ru>
2024-12-12 22:27:28 +0500

    added class Windows

---
+++ src/main/java/org/example/gui/Windows.java
@@ -0,0 +1,31 @@
+package org.example.gui;
+
+public class Windows {
+    private void renderNamesGenerators() {
+
+    }
+
+    private void renderDice() {
+
+    }
+
+    private void renderEncounterSettings() {
+
+    }
+
+    private void renderEncounterButtons() {
+
+    }
+
+    private void renderLeftWindow() {
+
+    }
+
+    private void renderRightWindow() {
+
+    }
+
+    public static void renderMainWindow() {
+
+    }
+}
