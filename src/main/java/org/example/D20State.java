c1c55b5e4e79dfb48fb806aaf9c5d30b040f7a2c
Axx <Sashik95@yandex.ru>
2024-12-12 22:40:46 +0500

    added the ability to get a string representation for enums

--- src/main/java/org/example/D20State.java
+++ src/main/java/org/example/D20State.java
@@ -1,5 +1,9 @@
 package org.example;
 
 public enum D20State {
-    NORMAL, ADVANTAGE, DISADVANTAGE
+    NORMAL, ADVANTAGE, DISADVANTAGE;
+
+    public static String[] getStringValues() {
+        return new String[] { NORMAL.name(), ADVANTAGE.name(), DISADVANTAGE.name() };
+    }
 }
--- src/main/java/org/example/EncounterDifficulty.java
+++ src/main/java/org/example/EncounterDifficulty.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public enum EncounterDifficulty {
-    EASY,
-    NORMAL,
-    HARD
+    EASY, NORMAL, HARD;
+
+    public static String[] getStringValues() {
+        return new String[] { EASY.name(), NORMAL.name(), HARD.name() };
+    }
 }
