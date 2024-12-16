a8368ce0bfd89d578ad274e2e1808772ae0a5332
Axx <Sashik95@yandex.ru>
2024-12-16 23:07:01 +0500

    adaptive getStringValues for D20State and EncounterDifficulty enums

--- src/main/java/org/example/enums/EncounterDifficulty.java
+++ src/main/java/org/example/enums/EncounterDifficulty.java
@@ -1,9 +1,13 @@
 package org.example.enums;
 
+import java.util.Arrays;
+
 public enum EncounterDifficulty {
     EASY, NORMAL, HARD;
 
     public static String[] getStringValues() {
-        return new String[] { EASY.name(), NORMAL.name(), HARD.name() };
+        return Arrays.stream(EncounterDifficulty.values())
+            .map(Enum::name)
+            .toArray(String[]::new);
     }
 }
