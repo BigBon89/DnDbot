72e1f770148afc59c009f17afff5853e4bec78b4
Axx <Sashik95@yandex.ru>
2024-12-17 00:33:01 +0500

    code style update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,12 @@ import org.example.namegenerators.ClassNameGenerator;
 public class Main {
     public static void main(String[] args) {
         BotLogicalCore botLogicalCore = new BotLogicalCore(
-                new CityNameGenerator(),
-                new CharacterNameGenerator(),
-                new ClassNameGenerator(),
-                new Dice(),
-                new Console(),
-                new Encounter()
+            new CityNameGenerator(),
+            new CharacterNameGenerator(),
+            new ClassNameGenerator(),
+            new Dice(),
+            new Console(),
+            new Encounter()
         );
 
         if (args.length > 0 && args[0].equalsIgnoreCase("console")) {
